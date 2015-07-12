fe4a4489baccc04de246f1c576b312e06ef1fb1a
Steve Tranby <stevetranby@gmail.com>
2015-07-12 02:50:28 -0600

    STEVE - allow public access to GLFWmonitor* _monitor

--- cocos/platform/desktop/CCGLViewImpl-desktop.h
+++ cocos/platform/desktop/CCGLViewImpl-desktop.h
@@ -80,6 +80,10 @@ public:
     bool windowShouldClose() override;
     void pollEvents() override;
     GLFWwindow* getWindow() const { return _mainWindow; }
+    
+    // STEVE
+    GLFWmonitor* getMonitor() const { return _monitor; }
+    
 
     bool isFullscreen() const;
     void setFullscreen();
