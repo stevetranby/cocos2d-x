5ffc153e5d080cfdd086473f36b50f21b678d155
Steve Tranby <stevetranby@gmail.com>
2015-09-07 18:45:34 -0600

    just copy by assign instead of std::copy

--- cocos/base/CCEventDispatcher.cpp
+++ cocos/base/CCEventDispatcher.cpp
@@ -961,8 +961,7 @@ void EventDispatcher::dispatchTouchEvent(EventTouch* event)
     bool isNeedsMutableSet = (oneByOneListeners && allAtOnceListeners);
     
     const std::vector<Touch*>& originalTouches = event->getTouches();
-    std::vector<Touch*> mutableTouches(originalTouches.size());
-    std::copy(originalTouches.begin(), originalTouches.end(), mutableTouches.begin());
+    std::vector<Touch*> mutableTouches(originalTouches);
 
     //
     // process the target handlers 1st
