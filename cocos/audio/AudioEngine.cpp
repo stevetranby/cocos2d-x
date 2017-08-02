f70d7d5e91671c0a0ad17ecaae716aa7a86169f5
Steve Tranby <stevetranby@gmail.com>
2017-08-02 06:31:28 -0600

    audio engine debug logging

--- cocos/audio/AudioEngine.cpp
+++ cocos/audio/AudioEngine.cpp
@@ -209,28 +209,28 @@ int AudioEngine::play2d(const std::string& filePath, bool loop, float volume, co
         // Steve: added check for null profileHelper
         if (profile && (!profileHelper || profile != &profileHelper->profile))
         {
-            CC_ASSERT(!profile->name.empty());
+            CCLOG("profile = %p, profileHelper = %p, profile->name = %s", profile, profileHelper, profile->name.c_str());
             profileHelper = &_audioPathProfileHelperMap[profile->name];
             profileHelper->profile = *profile;
         } else {
             // TODO: remove for release
-            log("profile = %p, profileHelper = %p", profile, profileHelper);
+            CCLOG("profile = %p, profileHelper = %p", profile, profileHelper);
         }
         
         if (_audioIDInfoMap.size() >= _maxInstances) {
-            log("Fail to play %s cause by limited max instance of AudioEngine",filePath.c_str());
+            CCLOG("Fail to play %s cause by limited max instance of AudioEngine",filePath.c_str());
             break;
         }
         if (profileHelper)
         {
              if(profileHelper->profile.maxInstances != 0 && profileHelper->audioIDs.size() >= profileHelper->profile.maxInstances){
-                 log("Fail to play %s cause by limited max instance of AudioProfile",filePath.c_str());
+                 CCLOGINFO("Fail to play %s cause by limited max instance of AudioProfile",filePath.c_str());
                  break;
              }
              if (profileHelper->profile.minDelay > TIME_DELAY_PRECISION) {
                  auto currTime = utils::gettime();
                  if (profileHelper->lastPlayTime > TIME_DELAY_PRECISION && currTime - profileHelper->lastPlayTime <= profileHelper->profile.minDelay) {
-                     log("Fail to play %s cause by limited minimum delay",filePath.c_str());
+                     CCLOGINFO("Fail to play %s cause by limited minimum delay",filePath.c_str());
                      break;
                  }
              }
