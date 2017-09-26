9e0fd6bca938fb6b4f1cee0bc3ef33b264a53ee5
Steve Tranby <stevetranby@gmail.com>
2017-09-25 20:43:30 -0600

    minor null check

--- cocos/2d/CCProgressTimer.cpp
+++ cocos/2d/CCProgressTimer.cpp
@@ -114,8 +114,10 @@ void ProgressTimer::setSprite(Sprite *sprite)
         CC_SAFE_RETAIN(sprite);
         CC_SAFE_RELEASE(_sprite);
         _sprite = sprite;
-        setContentSize(_sprite->getContentSize());
-
+        if (_sprite) {
+            setContentSize(_sprite->getContentSize());
+        }
+        
         //    Every time we set a new sprite, we free the current vertex data
         if (_vertexData)
         {
