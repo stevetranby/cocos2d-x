8af4e3b93e1528699e2fdf38aa585215b0e07ebf
Steve Tranby <stevetranby@gmail.com>
2018-12-03 14:26:52 -0700

    STEVE: removing more unused classes for build times

--- cocos/editor-support/cocosbuilder/CCNodeLoaderLibrary.cpp
+++ cocos/editor-support/cocosbuilder/CCNodeLoaderLibrary.cpp
@@ -26,18 +26,18 @@
 
 #include "editor-support/cocosbuilder/CCLayerLoader.h"
 #include "editor-support/cocosbuilder/CCLayerColorLoader.h"
-#include "editor-support/cocosbuilder/CCLayerGradientLoader.h"
+//#include "editor-support/cocosbuilder/CCLayerGradientLoader.h"
 #include "editor-support/cocosbuilder/CCLabelBMFontLoader.h"
-#include "editor-support/cocosbuilder/CCLabelTTFLoader.h"
+//#include "editor-support/cocosbuilder/CCLabelTTFLoader.h"
 #include "editor-support/cocosbuilder/CCSpriteLoader.h"
-#include "editor-support/cocosbuilder/CCScale9SpriteLoader.h"
+//#include "editor-support/cocosbuilder/CCScale9SpriteLoader.h"
 #include "editor-support/cocosbuilder/CCBFileLoader.h"
 #include "editor-support/cocosbuilder/CCMenuLoader.h"
 #include "editor-support/cocosbuilder/CCMenuItemLoader.h"
 #include "editor-support/cocosbuilder/CCMenuItemImageLoader.h"
-#include "editor-support/cocosbuilder/CCControlButtonLoader.h"
-#include "editor-support/cocosbuilder/CCParticleSystemQuadLoader.h"
-#include "editor-support/cocosbuilder/CCScrollViewLoader.h"
+//#include "editor-support/cocosbuilder/CCControlButtonLoader.h"
+//#include "editor-support/cocosbuilder/CCParticleSystemQuadLoader.h"
+//#include "editor-support/cocosbuilder/CCScrollViewLoader.h"
 
 using namespace cocos2d;
 
@@ -55,17 +55,17 @@ void NodeLoaderLibrary::registerDefaultNodeLoaders() {
     this->registerNodeLoader("CCNode", NodeLoader::loader());
     this->registerNodeLoader("CCLayer", LayerLoader::loader());
     this->registerNodeLoader("CCLayerColor", LayerColorLoader::loader());
-    this->registerNodeLoader("CCLayerGradient", LayerGradientLoader::loader());
+//    this->registerNodeLoader("CCLayerGradient", LayerGradientLoader::loader());
     this->registerNodeLoader("CCSprite", SpriteLoader::loader());
     this->registerNodeLoader("CCLabelBMFont", LabelBMFontLoader::loader());
-    this->registerNodeLoader("CCLabelTTF", LabelTTFLoader::loader());
-    this->registerNodeLoader("CCScale9Sprite", Scale9SpriteLoader::loader());
-    this->registerNodeLoader("CCScrollView", ScrollViewLoader::loader());
+//    this->registerNodeLoader("CCLabelTTF", LabelTTFLoader::loader());
+//    this->registerNodeLoader("CCScale9Sprite", Scale9SpriteLoader::loader());
+//    this->registerNodeLoader("CCScrollView", ScrollViewLoader::loader());
     this->registerNodeLoader("CCBFile", CCBFileLoader::loader());
     this->registerNodeLoader("CCMenu", MenuLoader::loader());
     this->registerNodeLoader("CCMenuItemImage", MenuItemImageLoader::loader());
-    this->registerNodeLoader("CCControlButton", ControlButtonLoader::loader());
-    this->registerNodeLoader("CCParticleSystemQuad", ParticleSystemQuadLoader::loader());
+//    this->registerNodeLoader("CCControlButton", ControlButtonLoader::loader());
+//    this->registerNodeLoader("CCParticleSystemQuad", ParticleSystemQuadLoader::loader());
 }
 
 void NodeLoaderLibrary::registerNodeLoader(const char * pClassName, NodeLoader * pNodeLoader) {
