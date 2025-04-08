b5957bfaa01d3fb5725275e5bf4288a89e3f5f5d
Steve <steve@alumni.colostate.edu>
2025-04-08 01:31:57 -0600

    STEVE: NOPUSH - game engine changes - Windowing Events

--- cocos/platform/desktop/CCGLViewImpl-desktop.h
+++ cocos/platform/desktop/CCGLViewImpl-desktop.h
@@ -176,6 +176,10 @@ protected:
     bool _isRetinaEnabled;
     int  _retinaFactor;  // Should be 1 or 2
 
+    // STEVE
+    cocos2d::Size _monitorSize;
+    bool _preventCharCallback;
+
     float _frameZoomFactor;
 
     GLFWwindow* _mainWindow;
@@ -190,6 +194,12 @@ protected:
 
     friend class GLFWEventHandler;
     
+public:
+    // View will trigger an event when window is resized, gains or loses focus
+    static const std::string EVENT_WINDOW_RESIZED;
+    static const std::string EVENT_WINDOW_FOCUSED;
+    static const std::string EVENT_WINDOW_UNFOCUSED;
+
 public:
     // View will trigger an event when window is resized, gains or loses focus
     static const std::string EVENT_WINDOW_RESIZED;
