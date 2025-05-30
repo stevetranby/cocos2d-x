3918140080ab7ae535809b018b23862f859a3519
Steve <steve@alumni.colostate.edu>
2025-05-30 15:56:06 -0600

    STEVE - allow public access to ListView curSelectionIndex

--- cocos/ui/UIListView.h
+++ cocos/ui/UIListView.h
@@ -525,6 +525,10 @@ protected:
 #pragma warning (pop)
 #endif
     ccListViewCallback _eventCallback;
+
+    // STEVE
+public:
+    void setCurSelectedIndex(ssize_t index) { _curSelectedIndex = index; }
 };
 
 }
