a71bad72cc2b80b11dc0abcc540d0c30cc4ec82e
Steve <steve@alumni.colostate.edu>
2025-05-30 21:42:30 -0600

    STEVE - //// to ---- and whitespace formatting

--- cocos/2d/CCAnimationCache.h
+++ cocos/2d/CCAnimationCache.h
@@ -141,7 +141,6 @@ private:
     Map<std::string, Animation*> _animations;
     static AnimationCache* s_sharedAnimationCache;
 
-
     // STEVE
 public:
     void addAnimationWithName(const std::string& animName, const std::string& animFrameName, const std::vector<std::string>& animFrameIndicies, float frameDelay = 0.1f);
