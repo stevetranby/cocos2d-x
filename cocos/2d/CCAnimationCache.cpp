06a653620443b27d67955d9b51aca34d74177344
Steve <steve@alumni.colostate.edu>
2025-05-30 21:42:21 -0600

    STEVE - //// to ---- and whitespace formatting

--- cocos/2d/CCAnimationCache.cpp
+++ cocos/2d/CCAnimationCache.cpp
@@ -247,8 +247,7 @@ void AnimationCache::addAnimationsWithFile(const std::string& plist)
 }
 
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------
 // MARK - STEVE Extensions
 
 void AnimationCache::addAnimationWithName(const string& animName, const string& animFrameName, const vector<string>& animFrameIndicies, float frameDelay)
